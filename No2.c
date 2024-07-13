d5c69a9c922b85e3af313e5664c89e82b2fa9343
Priyam Kalita <priyamkalita2002@gmail.com>
2024-07-13 08:45:59 +0530

    Initial Commit

---
+++ No2.c
@@ -0,0 +1,26 @@
+// Unix system calls
+// opendir, readdir
+
+#include <stdio.h>
+#include <dirent.h>
+
+int main()
+{
+          struct dirent *de; 
+
+          DIR *dr = opendir(".");
+
+          if (dr == NULL)
+          {
+                   printf("Could not open current directory" );
+                   return 0;
+          }
+
+          while ((de = readdir(dr)) != NULL)
+                             printf("%s\n", de->d_name);
+
+          closedir(dr);
+          return 1;
+
+
+}
